f50d74eac9c5d8c43ee2d25825838027b2e9ee46
shpolsky <shpolsky@gmail.com>
2012-03-16 15:02:22 +0600

    cf - 159D
    algo-class - Q1

---
+++ algo-class/ProgQ1/ProgQ1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 100000;
+
+long long mergeWithCount(vector<int> &a, int l, int r) {
+	vector<int> result;
+	int mid = (l + r) / 2, 
+		i = l, 
+		j = mid + 1, 
+		size = r - l + 1;
+	long long count = 0;
+	for (int k = 0; k < size; k++) {
+		if (j > r || i <= mid && a[i] < a[j]) {
+			result.push_back(a[i]);
+			i++;
+		} else {
+			result.push_back(a[j]);
+			j++;
+			if (i <= mid) count += mid - i + 1;
+		}
+	}
+	for (i = l; i <= r; i++) a[i] = result[i-l];
+	return count;
+}
+
+long long sortWithCount(vector<int> &a, int l, int r) {
+	if (l < r) {
+		int mid = (l + r) / 2;
+		int x = sortWithCount(a, l, mid);
+		int y = sortWithCount(a, mid + 1, r);
+		return x + y + mergeWithCount(a, l, r);
+	}
+	return 0;
+}
+
+int main() {
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+
+	vector<int> a(N);
+	for (int i = 0; i < N; i++) cin >> a[i];
+
+	cout << sortWithCount(a, 0, N-1);
+	
+	return 0;
+}
--- codeforces/D/159D/Pairs_159D.cpp
+++ codeforces/D/159D/Pairs_159D.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
+const int N = 2000;
+
 int main() {
-	freopen("input.txt", "rt", stdin);
-	freopen("output.txt", "wt", stdout);
+	//freopen("input.txt", "wt", stdout);
+	//for (int k = 0; k < 2000; k++) cout << 'z';
 
+	/*freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);*/
 
+	int i, j, x, y;
+	string s;
+	cin >> s;
+	int L = s.length();
+
+	vector<long long> less(N, 0), greater(N, 0);
+	for (i = 0; i < L; i++) {
+		for (x = y = i; x >= 0 && y < L && s[x] == s[y]; x--, y++) {
+			less[y]++; 
+			greater[x]++;
+		}
+
+		for (x = i, y = i + 1; x >= 0 && y < L && s[x] == s[y]; x--, y++) {
+			less[y]++; 
+			greater[x]++;
+		}
+	}
+
+	long long res = 0;
+	for (i = 0; i < L-1; i++) {
+		y = L-1;
+		long long g = 0;
+		while (y > i) { g += greater[y]; y--; }
+		res += less[i]*g;
+	}
+
+	cout << res;
 
 	return 0;
 }
